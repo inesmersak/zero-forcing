e59f80cd263e341f06dcdbe37263534210665065
Ines Mersak <inesmersak@gmail.com>
2019-07-23 22:59:58 +0200

    Dodala kodo za preverjanje s counterjem in Makefile. Hitrost se vedno slabsa od naivnega

--- koda/check.cpp
+++ koda/check.cpp
@@ -76,6 +76,51 @@ bool check_zero_forcing_set_queue_set(const Graph& graph, const vector<int>& zfs
     return all_vertices_coloured(colouring);
 }
 
+bool check_zero_forcing_set_queue_count(const Graph& graph, const vector<int>& zfs) {
+    vector<int> colouring(graph.vertices_num(), 1);
+    vector<int> forces(graph.vertices_num(), -2);  // -2 means the vertex hasn't been forced yet
+    for (int u : zfs) {
+        assert(u >= 0  && "zero-forcing set should contain vertex labels greater or equal to 0");
+        assert(u < graph.vertices_num() && "zero-forcing set should contain vertex labels smaller than graph size");
+        colouring[u] = 0;  // 0 means the vertex is coloured / black
+        forces[u] = -1;  // u is in starting set
+    }
+
+    queue<int> q;
+    vector<int> white_neighbours_cnt(graph.vertices_num(), 0);
+    // Preprocessing
+    for (int u=0; u < graph.vertices_num(); ++u) {
+        auto neighbours = find_white_neighbours(graph, u, colouring);
+        white_neighbours_cnt[u] = neighbours.size();
+        if (colouring[u] == 0 && white_neighbours_cnt[u] == 1) {
+            int v = neighbours[0];
+            if (forces[v] == -2) {
+                q.push(v);
+                forces[v] = u;
+            }
+        }
+    }
+
+    while (q.size() > 0) {
+        int u = q.front();
+        q.pop();
+        colouring[u] = 0;
+        for (int v : graph.adjacency_lists[u]) {
+            --white_neighbours_cnt[v];
+            if (colouring[v] == 0 && white_neighbours_cnt[v] == 1) {
+                auto neighbours = find_white_neighbours(graph, v, colouring);
+                int w = neighbours[0];
+                if (forces[w] == -2) {
+                    q.push(w);
+                    forces[w] = v;
+                }
+            }
+        }
+    }
+
+    return all_vertices_coloured(colouring);
+}
+
 int main () {
 /*     Graph K5 = Graph::complete_graph(24);
     Graph C4 = Graph::cycle(4);
@@ -84,7 +129,7 @@ int main () {
     bool r = timeit(check_zero_forcing_set_naive, K5, vector<int> {1,2,3,4}); */
 
     double p = 0.6;
-    vector<int> Ns = {10, 22, 46, 100, 215, 464, 1000, 2154, 4642, 10000};
+    vector<int> Ns = {10, 22, 46, 100, 215, 464, 1000, 2154, 4642, 10000, 15000};
     vector<Graph> graphs;
     vector<vector<int>> zfs;
     for (int N : Ns) {
@@ -94,10 +139,11 @@ int main () {
 
     for (int i=0; i < Ns.size(); ++i) {
         cout << "---------------------\n";
-        cout << "Graph size: " << Ns[i];
+        cout << "Graph size: " << Ns[i] << "\n";
         /* cout << graphs[i]; */
         timeit(check_zero_forcing_set_naive, graphs[i], zfs[i]);
-        timeit(check_zero_forcing_set_queue_set, graphs[i], zfs[i]);
+        // timeit(check_zero_forcing_set_queue_set, graphs[i], zfs[i]);
+        timeit(check_zero_forcing_set_queue_count, graphs[i], zfs[i]);
     }
 
     return 0;
